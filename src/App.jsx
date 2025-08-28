03daf17e0528e7eee7218df22039055598de6663
Sonal Kumar Keshri <sunny.keshri1990@gmail.com>
2025-08-28 18:58:05 +0530

    Todo Logic & UI implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,109 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import editLogo from './assets/edit.png';
+import deleteLogo from './assets/delete.png';
+import checkLogo from './assets/check.png';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userInput, setUserInput] = useState("");
+  const [list, setList] = useState([]);
+  const [temp, setTemp] = useState("");
+
+  const onChange = (e) => {
+    const value = e.target.value;
+    setUserInput(value);
+  }
+
+  const onSubmit = () => {
+    const newList = [...list];
+    newList.push({value: userInput, edit: false});
+    setList(newList);
+    setUserInput("");
+  }
+
+  const onEdit = (key) => {
+    let newList = list.map((item, index) => {
+      if(index===key)
+        return {
+          ...item,
+          edit: true
+        };
+      else 
+        return {
+          ...item,
+        edit: false
+      }
+    });
+    setList(newList);
+    setTemp(list[key].value);
+  }
+
+  const onDelete = (key) => {
+    const newList = [...list];
+    newList.splice(key,1);
+    setList(newList);
+  }
+
+  const onEditValue = (e) => {
+    setTemp(e.target.value);
+  }
+  const onUpdate = (key) => {
+    const newList = list.map((item, index) => {
+      if(index === key) {
+        return {
+          ...item,
+          value: temp,
+          edit: false,
+        }
+      } else {
+        return item;
+      }
+    });
+    setList(newList);
+  }
 
   return (
-    <>
+    <div className='container'>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h1>Todo List</h1>
+        <input 
+          type="text"
+          className="text"
+          value={userInput}
+          onChange={onChange}
+        />
+        <br />
+        <button 
+          onClick={onSubmit}
+          className="add"
+        >
+          Add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div>
+       { list.map((item, key) => 
+        <div className="itemView" key={key}>
+          {!(item.edit) ? <div clasname="displayView">
+            <p className="itemValue">{item.value}</p>
+            <button className="action" onClick={() => onEdit(key)}>
+              <img src={editLogo} />
+            </button> 
+            <button className="action" onClick={() => onDelete(key)}>
+              <img src={deleteLogo}/>
+            </button>
+          </div>
+          :
+          <div className='editView'>
+            <input className='textEdit' id="textEdit" value={temp} onChange={onEditValue} />
+              <button className="action" onClick={() => onUpdate(key)}>
+                <img src={checkLogo}/>
+              </button>
+          </div>}
+
+        </div>
+        ) }
+      </div>
+    </div>
   )
 }
 
